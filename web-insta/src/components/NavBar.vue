49397128899350c7cdccb65634475d45563ddf48
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-14 18:43:57 +0900

    Update NavBar.vue

--- web-insta/src/components/NavBar.vue
+++ web-insta/src/components/NavBar.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    <h1>nav</h1>
-    <button>Instagram</button>
-    <input type="text">
-    <ul>
-      <li><button>Home</button></li>
-      <li><button>DM</button></li>
-      <li><button>Create</button></li>
-      <li><button>Explore</button></li>
-      <li><button>Notice</button></li>
-      <li><button>Profile</button></li>
-    </ul>
+  <div class="nav-bar">
+    <b-navbar toggleable="lg" type="dark" variant="secondary">
+      <b-navbar-brand href="#">Instagram</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+    
+        <b-navbar-nav>
+          <b-nav-item href="#">Home</b-nav-item>
+          <b-nav-item href="#">DM</b-nav-item>
+          <b-nav-item href="#">Create</b-nav-item>
+          <b-nav-item href="#">Explore</b-nav-item>
+          <b-nav-item href="#">Notice</b-nav-item>
+          <b-nav-item href="#">Profile</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
@@ -21,5 +32,6 @@ export default {
 </script>
 
 <style>
+
 
 </style>
