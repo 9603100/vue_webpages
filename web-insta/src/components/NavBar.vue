7e25b07eb5466be82f55233dabb9a46b0d5e89cd
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-14 18:34:18 +0900

    Draft nav-bar

--- web-insta/src/components/NavBar.vue
+++ web-insta/src/components/NavBar.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <h1>nav</h1>
+    <button>Instagram</button>
+    <input type="text">
+    <ul>
+      <li><button>Home</button></li>
+      <li><button>DM</button></li>
+      <li><button>Create</button></li>
+      <li><button>Explore</button></li>
+      <li><button>Notice</button></li>
+      <li><button>Profile</button></li>
+    </ul>
   </div>
 </template>
 
