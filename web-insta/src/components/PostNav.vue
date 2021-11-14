5f45433ce9f8eae0e8f5f7602af9e2c10c93a58f
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-14 19:39:13 +0900

    Draft post-nav

---
+++ web-insta/src/components/PostNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="mb-4">
+      <b-avatar variant="info" src="https://placekitten.com/300/300"></b-avatar>
+    </div>
+    
+    <a href="">meongdi_</a>
+
+    <div>
+
+      <button @click="modalShow = !modalShow">...</button>
+
+      <b-modal v-model="modalShow">
+        <b-list-group>
+          <b-list-group-item>신고</b-list-group-item>
+          <b-list-group-item>팔로우 취소</b-list-group-item>
+          <b-list-group-item>게시물로 이동</b-list-group-item>
+          <b-list-group-item>공유 대상...</b-list-group-item>
+          <b-list-group-item>링크 복사</b-list-group-item>
+          <b-list-group-item>퍼가기</b-list-group-item>
+          <b-list-group-item>취소</b-list-group-item>
+        </b-list-group>
+      </b-modal>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostNav',
+  data() {
+      return {
+        modalShow: false
+      }
+    }
+  
+}
+</script>
+
+<style>
+
+</style>
