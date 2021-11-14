d3d1a2965c808a390f8ab73406b57730fd176547
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-14 19:15:18 +0900

    Create post-image

--- web-insta/src/components/Post.vue
+++ web-insta/src/components/Post.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <post-nav></post-nav>
     <post-image></post-image>
   </div>
 </template>
 
 <script>
 import PostImage from '@/components/PostImage'
+import PostNav from '@/components/PostNav'
 
 export default {
   name: 'Post',
   components: {
     PostImage,
+    PostNav,
   }
 }
 
