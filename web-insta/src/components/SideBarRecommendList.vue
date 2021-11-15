7f59d0326d2f6ca1d159807d30c7cd6f593f0e29
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-15 22:00:01 +0900

    Draft SideBarRecommnedList

--- web-insta/src/components/SideBarRecommendList.vue
+++ web-insta/src/components/SideBarRecommendList.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    <h1>list</h1>
+    <div>
+      <b-avatar variant="info" src="https://avatars.githubusercontent.com/u/77233824?v=4" size="6rem"></b-avatar>
+      <div>
+        <p>hoya0415</p>
+        <p>가예님</p>
+      </div>
+      <button>팔로우</button>
+    </div>
+     <div>
+      <b-avatar variant="info" src="https://avatars.githubusercontent.com/u/16138767?v=4" size="6rem"></b-avatar>
+      <div>
+        <p>hjyoon</p>
+        <p>효전님</p>
+      </div>
+      <button>팔로우</button>
+    </div>
+   
   </div>
 </template>
 
