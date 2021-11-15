ce4b0d0aa1fc6c802508c7a4f3e7a027a00d6c2b
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-15 22:07:02 +0900

    Update PostResponse.vue

--- web-insta/src/components/PostResponse.vue
+++ web-insta/src/components/PostResponse.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
     <post-response-like></post-response-like>
+    <div>
+      <p>user_id</p>
+      <p>내용</p>
+    </div>
+    <post-response-comment></post-response-comment>
   </div>
 </template>
 
 <script>
 import PostResponseLike from '@/components/PostResponseLike'
+import PostResponseComment from '@/components/PostResponseComment'
 
 export default {
   name: 'PostResponse',
   components: {
     PostResponseLike,
+    PostResponseComment,
   }
 }
 </script>
