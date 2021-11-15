b3771abd90b657fd93a74965f5635591a86426be
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-15 21:38:46 +0900

    Draft SideBarUserAccount

--- web-insta/src/components/SideBarUserAccount.vue
+++ web-insta/src/components/SideBarUserAccount.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    <h1>user account</h1>
+    <b-avatar variant="info" src="http://archivenew.vop.co.kr/images/90952a5d266e2222a78d4eb07938162f/2018-05/26023757_DeDeCbRVQAAhzEb.jpg" size="6rem"></b-avatar>
+    <div>
+      <p>MyNameIsYejung</p>
+      <p>박예정</p>
+    </div>
+    <div class="ms-auto">
+      <button @click="modalShow = !modalShow">전환</button>
+      <b-modal v-model="modalShow">
+        <b-list-group>
+          <b-list-group-item>계정전환</b-list-group-item>
+        </b-list-group>
+      </b-modal>
+    </div>
+
   </div>
 </template>
 
 <script>
 export default {
   name: 'SideBarUserAccount',
+  data() {
+      return {
+        modalShow: false
+      }
+    }
+  
 }
 </script>
 
