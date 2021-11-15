f027e5f543b2ced973a6cd17d24dc7fc2cc68352
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-15 20:06:43 +0900

    Update post

--- web-insta/src/components/Post.vue
+++ web-insta/src/components/Post.vue
@@ -2,18 +2,21 @@
   <div>
     <post-nav></post-nav>
     <post-image></post-image>
+    <post-response></post-response>
   </div>
 </template>
 
 <script>
 import PostImage from '@/components/PostImage'
 import PostNav from '@/components/PostNav'
+import PostResponse from '@/components/PostResponse'
 
 export default {
   name: 'Post',
   components: {
     PostImage,
     PostNav,
+    PostResponse,
   }
 }
 
