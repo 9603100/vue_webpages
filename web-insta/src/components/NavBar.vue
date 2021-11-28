62016586beb68ea6e8b5b27cf66edfd797646f44
9603100 <87457698+9603100@users.noreply.github.com>
2021-11-28 19:26:03 +0900

    Update search form and nav items

--- web-insta/src/components/NavBar.vue
+++ web-insta/src/components/NavBar.vue
@@ -2,15 +2,15 @@
   <div class="nav-bar">
     <b-navbar toggleable="lg" class="navbar d-flex justify-content-between">
       <b-navbar-brand href="#" @click="moveToHome"><img src="@/assets/instagram.png" alt=""></b-navbar-brand>
-      <b-form-input size="sm" class="search-input" placeholder="검색"></b-form-input>
-      <b-navbar-nav class="d-flex">
+      <b-form-input size="sm" class="search-input bg-light" placeholder="검색"></b-form-input>
+      <div class="nav-bar-items d-flex align-items-center">
         <b-nav-item href="#" @click="moveToHome"><b-icon icon="house-door"></b-icon></b-nav-item>
         <b-nav-item href="#"><b-icon icon="cursor"></b-icon></b-nav-item>
         <b-nav-item href="#"><b-icon icon="plus-square"></b-icon></b-nav-item>
         <b-nav-item href="#"><b-icon icon="compass"></b-icon></b-nav-item>
         <b-nav-item href="#"><b-icon icon="heart"></b-icon></b-nav-item>
         <b-nav-item href="#" @click="moveToProfile"><b-avatar variant="info" :src="user.profile_img_url" size="1.5em"></b-avatar></b-nav-item>
-      </b-navbar-nav>
+      </div>
     </b-navbar>
   </div>
 </template>
@@ -38,7 +38,7 @@ export default {
 .navbar{
   background-color: #fff;
   border-bottom: 1px solid #dcdcdc;
-  padding: 8px 20px;
+  padding: 8px 15px;
   width: 100vw;
 }
 
@@ -46,5 +46,13 @@ export default {
   width: 30vw;
 }
 
+li {
+  list-style: none;
+}
+
+a {
+  color: black;
+  padding: 12px 12px;
+}
 
 </style>
